10ced970d1030714bd751c171a7bc94a5dc1be78
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-12-16 12:24:25 +0900

    feat: username localstorage에 저장

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
-const loginButton = document.querySelector("#login-form button")
+const greeting = document.querySelector("#greeting")
 
-function onLoginBtnClick() {
-    console.log("click!!")
-    console.log("hello", loginInput.value)
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
-loginButton.addEventListener("click", onLoginBtnClick)
+
+function onLoginSubmit(event) {
+    event.preventDefault() // 이벤트가 기본 동작을 못하도록 함
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const username = loginInput.value
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreeting(username)
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener("submit", onLoginSubmit)
+} else {
+    paintGreeting(savedUsername)
+}
