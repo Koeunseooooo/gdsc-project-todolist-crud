a47b41dd04bae19a4f72340b6112274754cee977
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-12-18 14:17:26 +0900

    feat: save Todos (localstorage 저장)

--- src/js/todo.js
+++ src/js/todo.js
@@ -6,10 +6,8 @@ const todos = []
 function handleButtonClick(event) {
     const targetedTodo = event.target.parentElement
     targetedTodo.remove()
-
-    // const targetedTodoSpan = event.target.parentElement.childNodes[0].innerText // 이러면 인덱스를 무조건 0으로 고정시켜야하는데
-    // console.log(targetedTodoSpan)
 }
+
 function paintTodo(todoNew) {
     const li = document.createElement("li")
     const span = document.createElement("span")
@@ -22,15 +20,23 @@ function paintTodo(todoNew) {
     button.addEventListener("click", handleButtonClick)
 }
 
-function saveTodo(todoNew) {
-    todos.push(todoNew)
+function saveTodo() {
+    localStorage.setItem("todos", JSON.stringify(todos))
 }
+
 function handleTodoSubmit(event) {
     event.preventDefault()
     const todoNew = todoInput.value
     todoInput.value = ""
     paintTodo(todoNew)
+    todos.push(todoNew)
     saveTodo(todoNew)
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit)
+const savedTodos = localStorage.getItem("todos")
+
+if (savedTodos != null) {
+    const parsedTodos = JSON.parse(savedTodos)
+    parsedTodos.forEach(paintTodo)
+}
