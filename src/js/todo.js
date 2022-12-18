b7f4671d02d6e05f0edee4935dba5fc43b639263
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-12-18 13:51:12 +0900

    feat: deleting toDos

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,19 +1,36 @@
 const todoForm = document.getElementById("todo-form")
 const todoInput = todoForm.querySelector("input")
 const todoList = document.getElementById("todo-list")
+const todos = []
 
+function handleButtonClick(event) {
+    const targetedTodo = event.target.parentElement
+    targetedTodo.remove()
+
+    // const targetedTodoSpan = event.target.parentElement.childNodes[0].innerText // 이러면 인덱스를 무조건 0으로 고정시켜야하는데
+    // console.log(targetedTodoSpan)
+}
 function paintTodo(todoNew) {
     const li = document.createElement("li")
     const span = document.createElement("span")
     li.appendChild(span)
+    const button = document.createElement("button")
+    li.appendChild(button)
     span.innerText = todoNew
+    button.innerText = "✖️"
     todoList.appendChild(li)
+    button.addEventListener("click", handleButtonClick)
+}
+
+function saveTodo(todoNew) {
+    todos.push(todoNew)
 }
 function handleTodoSubmit(event) {
     event.preventDefault()
     const todoNew = todoInput.value
     todoInput.value = ""
     paintTodo(todoNew)
+    saveTodo(todoNew)
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit)
